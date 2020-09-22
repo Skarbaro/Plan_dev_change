1fe5510d4e01c9dc415165936e642293eeed4cb5
Alaster Rom <alaster.rof@gmail.com>
2020-09-22 17:50:59 +0300

    fic

--- src/java/DifNumb.java
+++ src/java/DifNumb.java
@@ -1,12 +1,12 @@
 public class DifNumbers {
 
-   public static void main(String[] args) {
+   public static void main(String args[]) {
       int num1 = 5, num2 = 15, dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
    
-   public static void main(String[] args) {
+   public static void main(String args[]) {
       int red1 = 5,
 	  int red2 = 17, rif;
       rif = red1 - red2;
