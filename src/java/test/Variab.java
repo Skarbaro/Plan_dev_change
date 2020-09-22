59e879f61a2ca7c0912571831ef1d45757b97adb
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-22 17:16:33 +0300

    add file

---
+++ src/java/test/Variab.java
@@ -0,0 +1,16 @@
+package src.java.test;
+
+class Variab {
+	
+	private int adr, ter, glory;
+	
+	public static void main (String[] args) {
+		
+		double a = 3;
+		int c;
+		int b = 4;
+		
+		c = Math.sqrt (a* a + b* b);
+		System.out.printlng ("c = "+ c);
+	}
+}
