0cca4f97cf3e3deeab2b4ab8e90b4a77653525db
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-23 09:31:05 +0300

    add fis

--- src/java/DifNumb.java
+++ src/java/DifNumb.java
@@ -1,19 +1,10 @@
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int num1 = 5;
-	  int num2 = 15;
+      int num1 = 5, num2 = 15;
 	  int dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
-   
-   public static void main(String args[]) {
-      int red1 = 5,
-	  int red2 = 17, rif;
-      rif = red1 - red2;
-      System.out.println("Sum of these numbers: " + rif);
-   }
-  
 
 }
