2925ac61aca6325e3a011ddf6eac80606b9d94d7
Skarbero <Skarbero@gmail.com>
2020-09-29 14:44:46 +0300

    change

--- src/java/ClientWindow.java
+++ src/java/ClientWindow.java
@@ -1,17 +1,12 @@
 public class AddTwoNumbers {
 	
-	private String cat;
-	private String color;
+	private String cat, color;
 	
-	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, sum;
+	public static void main(String args[]) {
+		int num1 = 5;
+		int num2 = 15, sum,
 		sum = num1 + num2;
 		System.out.println("Sum of these numbers: " + sum);
 	}
-	
-	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, del;
-		del = num1 + num2;
-		System.out.println("Del of these numbers: " + del);
-	}
+
 }
