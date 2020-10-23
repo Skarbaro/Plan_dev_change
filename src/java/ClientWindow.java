27d1bc2c6d014b7222a356a5ae5a94f30b74f90f
Alex Romaniuk <alaster.rom@gmail.com>
2020-10-23 11:44:09 +0300

    add fix

--- src/java/ClientWindow.java
+++ src/java/ClientWindow.java
@@ -3,8 +3,7 @@ public class AddTwoNumbers {
 	private String cat, color;
 	
 	public static void main(String args[]) {
-		int num1 = 5;
-		int num2 = 15, sum,
+		int num1 = 5, num2 = 15, sum,
 		sum = num1 + num2;
 		System.out.println("Sum of these numbers: " + sum);
 	}
