c8c1750b733e727cf7b0bc141d946215a67217e3
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-11 09:18:52 +0200

    add 1 file

--- src/java/test/ClientWindow.java
+++ src/java/test/ClientWindow.java
@@ -1,13 +1,15 @@
+package src.java.test;
+
 public class AddTwoNumbers {
 	
 	int del;
 	
 	public static void main(String[] args) {
-		int num1 = 5;
-		int num2 = 17;
-		int sum;
+		
+		int num1 = 5, num2 = 17, sum;
 		
 		sum = num1 + num2;
+		
 		System.out.println("Sum of these numbers: " + sum);
 	}
 
