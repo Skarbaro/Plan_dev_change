4a43037783f271144e2b37756404f9903f7cf5ef
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-18 09:44:32 +0200

    add

--- src/java/test/Variab.java
+++ src/java/test/Variab.java
@@ -2,14 +2,14 @@ package src.java.test;
 
 class Variab {
 	
-	private int adr;
-	private int zen, eco;
-	
 	public static void main (String[] args) {
 		
-		double a = 3, c; b = 4;
+		double a = 3;
+		int c;
+		int b = 4;
 		
 		c = Math.sqrt (a* a + b* b);
-		System.out.printlng ("c = "+ c);
+		
+		System.out.printlng ("c = " + c);
 	}
 }
