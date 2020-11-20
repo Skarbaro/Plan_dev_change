ff217359b4501c452544d9c269b67b9e37538805
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-20 10:07:08 +0200

    add go

--- src/java/Client_1.java
+++ src/java/Client_1.java
@@ -1,7 +1,6 @@
 class Variables {
-	public static void main (String args []) {
-		double a = 3;
-		double b = 4;
+	public static void main (String[] args) {
+		double a = 3, b = 4;
 		double c;
 		c = Math.sqrt (a* a + b* b);
 		System.out.println ("c = "+ c);
