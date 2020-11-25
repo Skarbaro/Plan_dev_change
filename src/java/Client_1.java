416d54ce04db6fdc1a7934874b95f0bc66a88651
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-25 09:40:49 +0200

    фвв

--- src/java/Client_1.java
+++ src/java/Client_1.java
@@ -3,17 +3,14 @@ class Client_1 {
 	private int aret;
 	
 	public static void main (String[] args) {
-		double a = 3, b = 4;
+
+		private stage_a;
+
+		double a = 3;
+		int b = 4;
 		double c;
 		c = Math.sqrt (a* a + b* b);
 		System.out.println ("c = "+ c);
 	}
 	
-	public static void main (String[] args) {
-		double a = 3; 
-		int b = 4,
-		int c;
-		c = Math.sqrt (a* a + b* b);
-		System.out.println ("c = "+ c);
-	}
 }
