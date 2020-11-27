7aa0aa645de7c8216bd0839bd7e2b3a69ad6d07e
Skarbaro <alaster.skar@gmail.com>
2020-11-27 17:10:19 +0200

    up 1 file new branch

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -1,20 +1,12 @@
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int num1 = 5;
-	  int num2 = 15;
+      int num1 = 5, num2 = 15;
 	  int dif;
       dif = num1 - num2;
       logger.log("Sum of these numbers: " + dif);
    }
-   
-   public static void main(String[] args) {
-      int red1 = 5;
-	  int red2 = 17;
-	  int rif;
-      rif = red1 - red2;
-      logger.log("Sum of these numbers: " + rif);
-   }
+ 
    
    public static void main(String[] args) {
       int rede1 = 5, rede2 = 17;
