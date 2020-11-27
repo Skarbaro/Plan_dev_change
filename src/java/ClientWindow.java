723011ae20d9dc3fa9d37fc92e0723b99a102bc8
Skarbaro <alaster.skar@gmail.com>
2020-11-27 16:54:04 +0200

    up

--- src/java/ClientWindow.java
+++ src/java/ClientWindow.java
@@ -3,14 +3,9 @@ public class AddTwoNumbers {
 	int del, art;
 	
 	public static void main(String args[]) {
-		int num1 = 5, num2 = 15, sum,
+		int num1 = 5;
+		int num2 = 15, sum,
 		sum = num1 + num2;
-		System.out.println("Sum of these numbers: " + sum);
-	}
-	
-	public static void main(String args[]) {
-		int num1 = 5, nim2 = 17, sum;
-		sum = num1 + nim2;
 		System.out.println("Sum of these numbers: " + sum);
 	}
 
