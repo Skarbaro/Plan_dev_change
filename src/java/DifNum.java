1567a4c6c3e154f3c3fda133ebd65ba70a22085b
Alex Romaniuk <alaster.rom@gmail.com>
2021-01-21 09:08:29 +0200

    Update DifNum.java

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -2,16 +2,17 @@ public class DifNumbers {
 
    public static void main(String args[]) {
       
-	  int dif, num7 = 5, num2 = 15;
+	  int dif, num7 = 5;
+	  int num2 = 15;
       dif = num7 - num2;
-      logger.log("Sum of these numbers: " + dif);
+      System.out.println("Sum of these numbers: " + dif);
    }
    
    public static void main(String[] args) {
       int red1 = 5, red2 = 17;
 	  int rif;
       rif = red1 - red2;
-      System.out.println("Sum of these numbers: " + rif);
+      logger.log("Sum of these numbers: " + rif);
    }
    
 
