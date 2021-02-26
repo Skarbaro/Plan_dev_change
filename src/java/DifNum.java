a6a02cc5abd2fd82a612ccb793d578e7d8d8bec4
Alex Romaniuk <alaster.rom@gmail.com>
2021-02-26 15:24:06 +0200

    fix num int

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -3,7 +3,8 @@ public class DifNumbers {
 	private int ater = 23;
 
    public static void main(String[] args) {
-      int num1 = 5, num2 = 18;
+      int num1 = 5;
+	  int num2 = 18;
 	  int dif;
       dif = num1 + num2;
       System.out.println("Sum of these numbers: " + dif);
