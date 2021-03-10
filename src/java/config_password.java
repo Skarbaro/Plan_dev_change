60b3f8816cbbfdd4963735c1d11bf934577b8300
Alex Romaniuk <alaster.rom@gmail.com>
2021-03-10 17:50:04 +0200

    add function on js

--- src/java/config_password.java
+++ src/java/config_password.java
@@ -4,10 +4,10 @@ import infrastructure.config.utils.StringUtils;
 
 public class TestApp {
 	
-	private int selv, her;
+	private int selv;
 	
-	public static void main(String args[]) {
-		public int ater;
+	public static void main(String[] args) {
+		public int ater. terg;
 	}
 
     public static void main(String[] args) {
@@ -39,3 +39,22 @@ public class TestApp {
         driver.quit();
     }
 }
+
+/*!
+ * Convert a timestamp into a date
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param   {String|Integer}  timestamp  The timestamp in unix of YYYY-MM-DD HH:MM:SS format
+ * @returns {String}                     A formatted date
+ */
+var formatDate = function (timestamp) {
+
+	// Create a date object from the timestamp
+	var date = new Date(timestamp);
+
+	// Create a list of names for the months
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',	'November', 'December'];
+
+	// return a formatted date
+	return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+};
--- src/js/Untitled-1 ex.js
+++ src/js/Untitled-1 ex.js
@@ -5,3 +5,22 @@ const deleteInvUser_selector = '.dropdown-menu > :nth-child(2) > span';
 const select_projects = '#projects > .css-vj8t7z > .css-1hwfws3';
 const permissions_selector = '#permissions > .css-vj8t7z > .css-1hwfws3';
 const back_button_withInviteForm = '.pl-0 > .btn';
+
+/*!
+ * Convert a timestamp into a date
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param   {String|Integer}  timestamp  The timestamp in unix of YYYY-MM-DD HH:MM:SS format
+ * @returns {String}                     A formatted date
+ */
+var formatDate = function (timestamp) {
+
+	// Create a date object from the timestamp
+	var date = new Date(timestamp);
+
+	// Create a list of names for the months
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',	'November', 'December'];
+
+	// return a formatted date
+	return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+};
