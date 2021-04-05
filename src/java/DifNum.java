718641fb4f4e6c34abecb608289bdeb6d0a266b5
Alex Romaniuk <alaster.rom@gmail.com>
2021-04-05 16:29:22 +0300

    add DifNum.java

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -3,8 +3,10 @@ public class DifNumbers {
 	private int ater = 23;
 
    public static void main(String[] args) {
-
+	
 	  int dif;
+	  int num1 = 10;
+	  int num2 = 11;
       dif = num1 + num2;
       System.out.println("Sum of these numbers: " + dif);
    }
