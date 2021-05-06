da9db7957b54c5749f84514baaa41f2289178d89
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-06 18:44:05 +0300

    update 1 file java

--- src/java/life.java
+++ src/java/life.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
+import java.Scanner;
 
 private int ter;
 
 public class Life {
 	
-	public static void main(String[] args) {
-      int red1 = 5;
-	  int red2 = 17;
+	public static void main(String args[]) {
+      int red1 = 5, red2 = 17;
 	  int rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
