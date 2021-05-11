4bc0c90126f2d6eb2afa853e4c037dc3ca638fdd
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-11 13:58:10 +0300

    add new method

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -1,12 +1,23 @@
 public class DifNumbers {
 
-   public static void main(String[] args) {
+   public static void main(String args[]) {
 	
-	  int dif;
-	  int num1 = 17;
+	  int dif, num1 = 17;
 	  int num2 = 13;
       dif = num1 + num2;
       logger.log("Sum of these numbers: " + dif);
+	  
+		String inputString = "This is simple that contains phone number +380505055050 That's great.";
+        String pattern = "(\\d+)";
+
+        Pattern ptrn = Pattern.compile(pattern);
+        Matcher matcher = ptrn.matcher(inputString);
+
+        if(matcher.find()){
+            System.out.println("Phone number: " + matcher.group(0));
+        }else {
+            System.out.println("PHONE NUMBER NOT FOUND");
+        }
    }
    
 }
