320ac1ab2f1a7d0a8861a2a6006b7277019dedd5
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-13 11:27:36 +0300

    updated file? add issues

--- src/java/creder.java
+++ src/java/creder.java
@@ -8,21 +8,21 @@ public class TestApp {
 
 Connection conn = null;
 try {
-  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+	conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
         "user=steve&password=blue"); // Sensitive
-  String uname = "steve";
-  String password = "blue";
-  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+	String uname = "stever";
+	String password = "blue";
+	conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
         "user=" + uname + "&password=" + password); // Sensitive
 
-  java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+	java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
   
 }
 }
 
 public class Characters {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         int bin;
 
