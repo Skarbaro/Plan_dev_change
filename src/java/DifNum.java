3624883926bcb92ddb64f9b13f50a75cd9465e60
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-13 11:22:30 +0300

    updated, del file

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -2,11 +2,11 @@ public class DifNumbers {
 
    public static void main(String[] args) {
 	
-	  int dif;
+	  int daf;
 	  int num1 = 170;
 	  int num2 = 13;
       dif = num1 + num2;
-      logger.log("Sum of these numbers: " + dif);
+      logger.log("Sum of these numbers: " + daf);
 	  
 		String inputString = "This is simple that contains phone number +380505055050 That's great.";
         String pattern = "(\\d+)";
@@ -15,7 +15,7 @@ public class DifNumbers {
         Matcher matcher = ptrn.matcher(inputString);
 
         if(matcher.find()){
-            System.out.println("Phone number: " + matcher.group(0));
+            System.out.println("Phone number: " + matcher.group(1));
         }else {
             System.out.println("PHONE NUMBER NOT FOUND");
         }
