a6fcd353727b09bc4f3679b1c40a809a42d27327
Alex Romaniuk <alaster.rom@gmail.com>
2021-05-17 09:06:47 +0300

    updated fix issues and file

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -3,10 +3,10 @@ public class DifNumbers {
    public static void main(String[] args) {
 	
 	  int daf;
-	  int num1 = 17, num3 - 2;
-	  int num2 = 13;
-      dif = num1 + num2 + num3;
-      logger.log("Sum of these numbers: " + daf);
+	  int num1 = 19, num3 - 3;
+	  int num2 = 17;
+      dif = num1 + num2 - num3;
+      System.out.println("Sum of these numbers: " + daf);
 	  
 		String inputString = "This is simple that contains phone number +380505055050 That's great.";
         String pattern = "(\\d+)";
@@ -15,7 +15,7 @@ public class DifNumbers {
         Matcher matcher = ptrn.matcher(inputString);
 
         if(matcher.find()){
-            System.out.println("Phone number: " + matcher.group(1));
+            logger.log("Phone number: " + matcher.group(1));
         }else {
             System.out.println("PHONE NUMBER NOT FOUND");
         }
