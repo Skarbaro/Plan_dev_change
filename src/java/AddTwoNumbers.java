b2000f9ab137b8ac740a8ce077911abef9d0f24c
Alex Romaniuk <alaster.rom@gmail.com>
2021-06-10 11:30:37 +0300

    add file

---
+++ src/java/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+package src;
+
+public class AddTwoNumbers {
+   
+   public static void main(String[] args) {
+      
+      int num1 = 3;
+	  int num2 = 14;
+	  int dif;
+
+      del = num1 - num2;
+	  
+      logger.log("Dif of these numbers: " + dif);
+   }
+   
+}
