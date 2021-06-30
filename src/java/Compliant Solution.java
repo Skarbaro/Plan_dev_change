97d8f8407de000e910ea642274fdb6ccb3d6e6e9
Cayde- <Cayde-7.polk@gmail.com>
2021-06-30 11:54:51 +0300

    Create Compliant Solution.java

---
+++ src/java/Compliant Solution.java
@@ -0,0 +1,21 @@
+package src.java;
+
+public class DifNumbers {
+	
+	private int deo;
+   
+   public static void main(String args[]) {
+	   
+	   private int tel, get, red1 = 5, red2 = 10, rif;
+      rif = red1 - red2;
+      System.out.println("Sum of these numbers: " + rif);
+   }
+  
+ Connection conn = null;
+try {
+  String uname = getEncryptedUser();
+  String password = getEncryptedPass();
+  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+        "user=" + uname + "&password=" + password);
+}
+}
