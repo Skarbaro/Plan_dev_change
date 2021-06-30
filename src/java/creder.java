5c35dc567dec52b5b0c5ff91ed1f1dd54825d244
Cayde-6 <Cayde-6.polk@gmail.com>
2021-06-30 10:08:07 +0300

    Update creder.java

--- src/java/creder.java
+++ src/java/creder.java
@@ -4,7 +4,7 @@ import infrastructure.config.utils.StringUtils;
 
 public class TestApp {
 	
-	int atre = 30;
+	int atre = 17;
 
 Connection conn = null;
 try {
@@ -17,19 +17,20 @@ try {
 
 	java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
 	
-	private int c = 1;
+	private int c = 7;
   
 }
 }
 
 public class Characters {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        private int bin;
+        private int bon;
+		int cer = 14;
 
-        for(bint = 'B'; bin <= 'Z'; ++bin)
+        for(bin = 'B'; bin <= 'Z'; ++bin)
             	
-		System.out.print(bin + " " + ber);
+		System.out.print(bin + " " + bon + cer);
     }
 }
