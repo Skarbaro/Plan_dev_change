094da9bd14afed8c29ee49bad4a8f0477d24c364
Alex Romaniuk <alaster.rom@gmail.com>
2021-06-30 11:57:07 +0300

    no message

--- src/java/Compliant Solution.java
+++ src/java/Compliant Solution.java
@@ -3,15 +3,22 @@ package src.java;
 public class DifNumbers {
 	
 	private int deo;
+	
+	 public static void main(String args[]) {
+      int num1 = 7;
+	  int num2 = 16, dif;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
    
    public static void main(String args[]) {
 	   
-	   private int tel, get, red1 = 5, red2 = 10, rif;
+	   private int red1 = 5, red2 = 10, rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
    }
   
- Connection conn = null;
+ 
 try {
   String uname = getEncryptedUser();
   String password = getEncryptedPass();
