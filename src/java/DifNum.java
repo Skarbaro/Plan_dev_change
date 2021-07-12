54dce41a89c4a9743db8183f6daca05d2805fe66
Alex Romaniuk <alaster.rom@gmail.com>
2021-07-12 13:58:38 +0300

    no message

--- src/java/DifNum.java
+++ src/java/DifNum.java
@@ -4,12 +4,11 @@ public class DifNumbers {
 	
 	  private int difer;
 	  
-	  int num1 = 21, num3 = 7;
-	  int num2 = 17;
+	  int num1 = 21, num2 = 17;
 	  
       difer = num1 - num2;
       logger.log("Sum of these numbers: " + difer);
-	  System.out.println("Phone number: " + matcher.group(0));
+	  System.out.println("Phone number: " + matcher.group(1));
 	  
 		String inputString = "This is simple that contains phone number +380505055050 That's great.";
         String pattern = "(\\d+)";
